98a78c7d55f2a5a6ad1d198909166a62e29c7521
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-09-03 19:13:43 -0400

    Add ProcessExitHandler to monitor game exits

---
+++ src/DXVKCompanion/Monitoring/ProcessExitHandler.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Diagnostics;
+
+namespace DXVKCompanion.Monitoring
+{
+    public class ProcessExitHandler
+    {
+        public event Action<Process>? OnGameExited;
+
+        public void Attach(Process process)
+        {
+            try
+            {
+                process.EnableRaisingEvents = true;
+                process.Exited += (_, _) => OnGameExited?.Invoke(process);
+            }
+            catch
+            {
+                // Some protected processes cannot be hooked
+            }
+        }
+    }
+}
