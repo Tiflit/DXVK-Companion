6b4911ecdfe2480c441c73d8f256453b88b0d036
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-09-03 19:13:16 -0400

    Add ApiClassifier to classify graphics APIs

---
+++ src/DXVKCompanion/Monitoring/ApiClassifier.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using DXVKCompanion.Utils;
+
+namespace DXVKCompanion.Monitoring
+{
+    public enum GraphicsApi
+    {
+        Unknown,
+        DX9,
+        DX11,
+        ModernAPI
+    }
+
+    public class ApiClassifier
+    {
+        private readonly ModuleScanner _scanner;
+        private readonly PeParser _parser;
+
+        public ApiClassifier(ModuleScanner scanner, PeParser parser)
+        {
+            _scanner = scanner;
+            _parser = parser;
+        }
+
+        public GraphicsApi Classify(Process process)
+        {
+            if (_scanner.UsesDirectX(process))
+                return GraphicsApi.DX11; // Most DXVK-compatible games load DX11 dynamically
+
+            var imports = _parser.GetImports(process.MainModule.FileName);
+
+            if (imports.Contains("d3d9.dll"))
+                return GraphicsApi.DX9;
+
+            if (imports.Contains("d3d11.dll"))
+                return GraphicsApi.DX11;
+
+            if (imports.Contains("d3d12.dll") || imports.Contains("vulkan-1.dll"))
+                return GraphicsApi.ModernAPI;
+
+            return GraphicsApi.Unknown;
+        }
+    }
+}
