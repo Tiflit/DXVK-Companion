cbecdd0223a53c611e0cfcb65cdb1c511a36356d
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-09-03 20:29:56 -0400

    Refactor ProcessMonitor for thread safety and disposal
    Refactor ProcessMonitor to use ConcurrentDictionary for seen PIDs and implement IDisposable.

--- src/DXVKCompanion/Monitoring/ProcessMonitor.cs
+++ src/DXVKCompanion/Monitoring/ProcessMonitor.cs
@@ -1,14 +1,14 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 
 namespace DXVKCompanion.Monitoring
 {
-    public class ProcessMonitor
+    public class ProcessMonitor : IDisposable
     {
-        private readonly HashSet<int> _seenPids = new();
+        private readonly ConcurrentDictionary<int, byte> _seenPids = new();
         private readonly Timer _timer;
         private readonly GameDetector _detector;
         private readonly ProcessExitHandler _exitHandler;
@@ -20,24 +20,57 @@ namespace DXVKCompanion.Monitoring
             _detector = detector;
             _exitHandler = exitHandler;
 
+            // Poll every 2 seconds; low overhead for a tray app
             _timer = new Timer(PollProcesses, null, 0, 2000);
         }
 
         private void PollProcesses(object? state)
         {
-            foreach (var proc in Process.GetProcesses())
+            Process[] processes;
+
+            try
             {
-                if (_seenPids.Contains(proc.Id))
+                processes = Process.GetProcesses();
+            }
+            catch
+            {
+                return;
+            }
+
+            var currentPids = processes.Select(p => p.Id).ToHashSet();
+
+            // Cleanup: remove PIDs that no longer exist
+            foreach (var pid in _seenPids.Keys)
+            {
+                if (!currentPids.Contains(pid))
+                    _seenPids.TryRemove(pid, out _);
+            }
+
+            foreach (var proc in processes)
+            {
+                if (_seenPids.ContainsKey(proc.Id))
                     continue;
 
-                _seenPids.Add(proc.Id);
+                _seenPids[proc.Id] = 1;
 
-                if (_detector.IsGameProcess(proc))
+                if (!_detector.IsGameProcess(proc))
+                    continue;
+
+                try
                 {
                     _exitHandler.Attach(proc);
                     OnGameDetected?.Invoke(proc);
                 }
+                catch
+                {
+                    // Ignore failures for individual processes
+                }
             }
+        }
+
+        public void Dispose()
+        {
+            _timer.Dispose();
         }
     }
 }
